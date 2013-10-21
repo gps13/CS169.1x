e840fbb9ce726323044ae58977fb045dcd38031b
gps13 <g.psistakis@gmail.com>
2013-10-21 11:23:48 +0300

    hw1-2 ok

--- HW1/lib/dessert.rb
+++ HW1/lib/dessert.rb
@@ -1,17 +1,47 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name=name
+    @calories=calories
   end
   def healthy?
-    # your code here
+    @calories<200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @name = "#{flavor} jelly bean"
+    @calories = 5
+    @flavor = flavor
   end
+  def delicious?
+    @flavor != 'licorice'
+  end
+
 end
+
+cake = Dessert.new('cake', 400)
+puts cake.name
+puts cake.calories
+puts cake.healthy?
+puts cake.delicious?
+puts '---------------------------'
+apple = Dessert.new('apple', 75)
+puts apple.name
+puts apple.calories
+puts apple.healthy?
+puts apple.delicious?
+puts '---------------------------'
+jelly = JellyBean.new('vanilla')
+puts jelly.name
+puts jelly.calories
+puts jelly.healthy?
+puts jelly.delicious?
+puts '---------------------------'
+jelly2 = JellyBean.new('licorice')
+puts jelly2.name
+puts jelly2.delicious?
