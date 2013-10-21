99b26408037d9cfa9b7da73638486a2f0b760661
gps13 <g.psistakis@gmail.com>
2013-10-21 14:53:23 +0300

    hw1-3 ok

--- HW1/lib/rock_paper_scissors.rb
+++ HW1/lib/rock_paper_scissors.rb
@@ -4,11 +4,21 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    if ["R", "P", "S"].include?(player1[1].upcase) && ["R", "P", "S"].include?(player2[1].upcase)
+		player1[1] + player2[1] =~ /rs|sp|pr|rr|ss|pp/i ? player1 : player2
+    else
+    	raise RockPaperScissors::NoSuchStrategyError, "Strategy must be one of R,P,S" 
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+  	if tournament[0][0].is_a?(String)
+  		self.winner(tournament[0], tournament[1])
+  	else
+  	    p1 = self.tournament_winner(tournament[0])
+    	p2 = self.tournament_winner(tournament[1])
+    	self.tournament_winner([p1,p2])
+  	end
   end
 
 end
