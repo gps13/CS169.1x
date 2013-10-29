8e95556bf413d9e5a39b397f9fe6d0461372456b
gps13 <g.psistakis@gmail.com>
2013-10-30 01:58:54 +0200

    rottenpotatoes from scratch, init commit

---
+++ HW2/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '786392b547f872e0d2f4bbbd640bc86f40a551f9c5ee17240aecf76654eadea0e19a5c914bccd776ca4fa8b2d95718b2b2a905e524022f5cb30ff4dd3e3d2cdb'
